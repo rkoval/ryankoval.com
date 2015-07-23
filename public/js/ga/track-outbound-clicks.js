5f81552a86d515dedd766932dd8721ec4471a555
Ryan Koval <kovalrya@gmail.com>
2015-07-22 21:36:53 -0500

    moved google analytic scripts to their own files

---
+++ public/js/ga/track-outbound-clicks.js
@@ -0,0 +1,12 @@
+(function() {
+  var trackOutboundLink = function(url) {
+    ga('send', 'event', 'outbound', 'click', url, {'hitCallback': function () { document.location = url; } });
+  };
+  // dynamically append function to links with href
+  $(document).ready(function() {
+    var hrefLinks = $('a.tracked');
+    hrefLinks.each(function() {
+      $(this).attr('onclick', 'trackOutboundLink("' + this.href + '"); return false;');
+    });
+  });
+}());
