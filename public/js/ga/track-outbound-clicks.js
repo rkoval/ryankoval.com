adfb35be17c366a92901bf1ad08ee563092e2982
Ryan Koval <kovalrya@gmail.com>
2015-07-24 16:47:30 -0500

    fixed trackOutboundLink not working

--- public/js/ga/track-outbound-clicks.js
+++ public/js/ga/track-outbound-clicks.js
@@ -1,12 +1,10 @@
-(function() {
-  var trackOutboundLink = function(url) {
-    ga('send', 'event', 'outbound', 'click', url, {'hitCallback': function () { document.location = url; } });
-  };
-  // dynamically append function to links with href
-  $(document).ready(function() {
-    var hrefLinks = $('a.tracked');
-    hrefLinks.each(function() {
-      $(this).attr('onclick', 'trackOutboundLink("' + this.href + '"); return false;');
-    });
+var trackOutboundLink = function(url) {
+  ga('send', 'event', 'outbound', 'click', url, {'hitCallback': function () { document.location = url; } });
+};
+// dynamically append function to links with href
+$(document).ready(function() {
+  var hrefLinks = $('a.tracked');
+  hrefLinks.each(function() {
+    $(this).attr('onclick', 'trackOutboundLink("' + this.href + '"); return false;');
   });
-}());
+});
