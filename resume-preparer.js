a17c17c1e6d99748fc39fa2e82bf4706ea98179b
Ryan Koval <ryan@ryankoval.com>
2018-01-18 22:24:25 -0600

    added pdf generation to docker build

--- resume-preparer.js
+++ resume-preparer.js
@@ -2,6 +2,9 @@ import moment from 'moment-timezone'
 import _ from 'lodash'
 import resume from './src/resume.yml'
 
+import fontawesome from '@fortawesome/fontawesome'
+import { faFilePdf } from '@fortawesome/fontawesome-free-solid'
+
 // https://github.com/jantimon/html-webpack-plugin/issues/597#issuecomment-281663833
 export default () => {
   const template = require('./src/views/resume.pug')
@@ -10,6 +13,10 @@ export default () => {
       {
         moment,
         _,
+        iconStyles: fontawesome.dom.css(),
+        icons: {
+          faFilePdf: fontawesome.icon(faFilePdf).html[0],
+        },
       },
       resume
     )
