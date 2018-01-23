730edf1781c58584771fa1c519819899779dfa13
Ryan Koval <ryan@ryankoval.com>
2018-01-22 21:27:23 -0600

    added grouped skills to resume

--- resume-preparer.js
+++ resume-preparer.js
@@ -5,6 +5,24 @@ import resume from './src/resume.yml'
 import fontawesome from '@fortawesome/fontawesome'
 import { faFilePdf } from '@fortawesome/fontawesome-free-solid'
 
+const sort = [
+  'Languages',
+  'Infrastructure',
+  'Data Stores',
+  'Frameworks & Libraries',
+  'Automation',
+  'Miscellaneous',
+]
+
+resume.chunkedGroupedSkills = _.chain(resume.skills)
+  .map('skill')
+  .filter('resume')
+  .groupBy('tag')
+  .entries()
+  .sortBy(([group, skills]) => _.indexOf(sort, group))
+  .chunk(3)
+  .value()
+
 // https://github.com/jantimon/html-webpack-plugin/issues/597#issuecomment-281663833
 export default () => {
   const template = require('./src/views/resume.pug')
