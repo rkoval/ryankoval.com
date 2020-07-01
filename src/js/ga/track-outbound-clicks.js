0823edf7823a4bf79b0ebb6d9365873948514032
Ryan Koval <ryan@ryankoval.com>
2020-07-01 18:02:31 -0500

    prettier

--- src/js/ga/track-outbound-clicks.js
+++ src/js/ga/track-outbound-clicks.js
@@ -1,10 +1,17 @@
 var trackOutboundLink = function(url) {
-  ga('send', 'event', 'outbound', 'click', url, {'hitCallback': function () { document.location = url; } });
-};
+  ga('send', 'event', 'outbound', 'click', url, {
+    hitCallback: function() {
+      document.location = url
+    },
+  })
+}
 // dynamically append function to links with href
 $(document).ready(function() {
-  var hrefLinks = $('a.tracked');
+  var hrefLinks = $('a.tracked')
   hrefLinks.each(function() {
-    $(this).attr('onclick', 'trackOutboundLink("' + this.href + '"); return false;');
-  });
-});
+    $(this).attr(
+      'onclick',
+      'trackOutboundLink("' + this.href + '"); return false;'
+    )
+  })
+})
